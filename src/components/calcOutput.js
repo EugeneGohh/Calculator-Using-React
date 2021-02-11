7984d4c2f4218bf2d92894df4068adb3adceeaac
Eugene Goh <gzkgt11@gmail.com>
2021-02-12 01:29:49 +0800

    Updated using let

--- src/components/calcOutput.js
+++ src/components/calcOutput.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class CalcOutput extends Component {
   render() {
-    const { outcome } = this.props;
+    let { outcome } = this.props;
     return (
       <div className="result">
         <p>{outcome}</p>
