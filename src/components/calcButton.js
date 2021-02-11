c030afb3953812cb2b0acda599c76114ed4b383f
Eugene Goh <gzkgt11@gmail.com>
2021-02-11 18:41:33 +0800

    First Commit

---
+++ src/components/calcButton.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import CancelIcon from "@material-ui/icons/Cancel";
+
+export class CalcButton extends Component {
+  render() {
+    return (
+      <div className="button">
+        <button name="(" onClick={(e) => this.props.onClick(e.target.name)}>
+          (
+        </button>
+        <button name="CE" onClick={(e) => this.props.onClick(e.target.name)}>
+          <CancelIcon />
+        </button>
+        <button name=")" onClick={(e) => this.props.onClick(e.target.name)}>
+          )
+        </button>
+        <button name="C" onClick={(e) => this.props.onClick(e.target.name)}>
+          C
+        </button>
+
+        <button name="1" onClick={(e) => this.props.onClick(e.target.name)}>
+          1
+        </button>
+        <button name="2" onClick={(e) => this.props.onClick(e.target.name)}>
+          2
+        </button>
+        <button name="3" onClick={(e) => this.props.onClick(e.target.name)}>
+          3
+        </button>
+        <button name="+" onClick={(e) => this.props.onClick(e.target.name)}>
+          <AddIcon />
+        </button>
+
+        <button name="4" onClick={(e) => this.props.onClick(e.target.name)}>
+          4
+        </button>
+        <button name="5" onClick={(e) => this.props.onClick(e.target.name)}>
+          5
+        </button>
+        <button name="6" onClick={(e) => this.props.onClick(e.target.name)}>
+          6
+        </button>
+        <button name="-" onClick={(e) => this.props.onClick(e.target.name)}>
+          <RemoveIcon />
+        </button>
+
+        <button name="7" onClick={(e) => this.props.onClick(e.target.name)}>
+          7
+        </button>
+        <button name="8" onClick={(e) => this.props.onClick(e.target.name)}>
+          8
+        </button>
+        <button name="9" onClick={(e) => this.props.onClick(e.target.name)}>
+          9
+        </button>
+        <button name="*" onClick={(e) => this.props.onClick(e.target.name)}>
+          x
+        </button>
+
+        <button name="." onClick={(e) => this.props.onClick(e.target.name)}>
+          .
+        </button>
+        <button name="0" onClick={(e) => this.props.onClick(e.target.name)}>
+          0
+        </button>
+        <button name="=" onClick={(e) => this.props.onClick(e.target.name)}>
+          =
+        </button>
+        <button name="/" onClick={(e) => this.props.onClick(e.target.name)}>
+          ÷
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CalcButton;
